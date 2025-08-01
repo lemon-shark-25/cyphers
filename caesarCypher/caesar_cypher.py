b922d71891d2f48a1fc36269f40a14fcf080524c
lemon-shark <196343170+lemon-shark-25@users.noreply.github.com>
2025-08-01 15:43:34 +0200

    Add files via upload

---
+++ caesarCypher/caesar_cypher.py
@@ -0,0 +1,29 @@
+def caesarEncrypt(message, key=0):
+    # Initialize encrypted string
+    encrypted = ""
+
+    for i in range(len(message)):
+        char = message[i]
+
+        if (65 <= ord(char) <= 90):     # Check for uppercase 
+            encrypted += chr(65 + (ord(char) - 65 + key) % 26)
+        elif (97 <= ord(char) <= 122):  # Check for lowercase
+            encrypted += chr(97 + (ord(char) - 97 + key) % 26)
+        else:                           # Every other symbol
+            encrypted += char
+    return encrypted
+
+def caesarDecrypt(message, key=0):
+    # Initialize encrypted string
+    decrypted = ""
+
+    for i in range(len(message)):
+        char = message[i]
+
+        if (65 <= ord(char) <= 90):     # Check for uppercase 
+            decrypted += chr(65 + (ord(char) - 65 - key) % 26)
+        elif (97 <= ord(char) <= 122):  # Check for lowercase
+            decrypted += chr(97 + (ord(char) - 97 - key) % 26)
+        else:                           # Every other symbol
+            decrypted += char
+    return decrypted
